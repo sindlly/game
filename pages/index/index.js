52240dc85a581263225de652a20866a9e1c5efcc
nashdeng <nashdeng@tencent.com>
2021-04-19 19:35:43 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,8 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    showIntro:false,
+    showRecord:false,
   },
   // 事件处理函数
   bindViewTap() {
@@ -24,6 +20,19 @@ Page({
       })
     }
   },
+  togglleRule(){
+    this.setData({
+      showIntro:!this.data.showIntro
+    })
+  },
+  togglleRecord(){
+    this.setData({
+      showRecord:!this.data.showRecord
+    })
+  },
+
+
+
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
